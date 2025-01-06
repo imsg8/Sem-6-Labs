3bcebec9e85c8b400cf221fb5e015b4a63505623
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2025-01-06 13:41:26 +0530

    Add files via upload

---
+++ PCAP/Lab-1/Q2/q2.c
@@ -0,0 +1,25 @@
+// 2. Write a program in MPI where even ranked process prints "Hello"
+// and odd ranked process prints "World".
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<mpi.h>
+
+int main(int argc, char *argv[]){
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	if(rank % 2 == 0){
+		printf("Process %d : Hello", rank);
+	}
+	else{
+		printf("Process %d : World", rank);
+	}
+	printf("\n \n");
+    MPI_Finalize();
+    return 0;
+}
+
+// Sample Input:
+// mpirun -n 5 ./q2
