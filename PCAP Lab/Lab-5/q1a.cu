5f9919fe04b5800f57cf01f297d40ea8c193ce84
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2025-02-01 10:23:02 +0530

    Create q1a.cu

---
+++ PCAP Lab/Lab-5/q1a.cu
@@ -0,0 +1,47 @@
+// Credits: Tejinder Pokuri
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include<stdio.h>
+
+__global__ void add_vec(int*da,int*db,int*dc){
+    int index=blockIdx.x*blockDim.x+threadIdx.x;
+    dc[index]=da[index]+db[index];
+}
+
+int main(){
+    int n;
+    printf("Length of the vector : ");
+    scanf("%d",&n);
+
+    int a[n],b[n],c[n];
+    int *da,*db,*dc;
+
+    cudaMalloc((void **)&da,n*sizeof(int));
+    cudaMalloc((void **)&db,n*sizeof(int));
+    cudaMalloc((void **)&dc,n*sizeof(int));
+
+    printf("Enter vector one : ");
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    printf("Enter vector two : ");
+    for(int i=0;i<n;i++)
+        scanf("%d",&b[i]);
+    
+    cudaMemcpy(da,a,n*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(db,b,n*sizeof(int),cudaMemcpyHostToDevice);
+
+    dim3 grid(n,1,1);
+    dim3 blk(1,1,1);
+
+    add_vec<<<grid,blk>>>(da,db,dc);
+    cudaMemcpy(c,dc,n*sizeof(int),cudaMemcpyDeviceToHost);
+
+    for(int i=0;i<n;i++)
+        printf("%d\t",c[i]);
+    printf("\n");
+    cudaFree(da);
+    cudaFree(db);
+    cudaFree(dc);
+}
