251be33dc0ab65ff06748b447fb547cfd4b16f31
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2025-02-01 10:14:15 +0530

    Create q1.c

---
+++ PCAP Lab/Lab-5/q1.c
@@ -0,0 +1,12 @@
+Credits: Tejinder Pokuri
+
+#include<stdio.h>
+
+int main(){
+    for(int i=1;i<567;i++)
+        printf("%d ",i);
+    printf("\n");
+    for(int i=567;i>0;i--)
+        printf("%d ",i);
+    printf("\n");
+}
